a7d6a70fe256cbfb8b1c01760ed0fdf6bbb3b7f4
Justin Lubin <jlubi333@gmail.com>
2022-11-15 20:49:17 -0800

    Initial commit

---
+++ download-data.py
@@ -0,0 +1,1 @@
+import urllib.requestimport gzipimport csvwith open("data-sources.csv") as csv_file:    reader = csv.DictReader(csv_file)    for row in reader:        with urllib.request.urlopen(row["url"]) as data_gz_file:            f = gzip.GzipFile(fileobj=data_gz_file)            with open("cached-data/" + row["name"], "wb") as data_file:                data_file.write(f.read())
