36a3258c8e94e5911d8589621865c2d753069966
Justin Lubin <jlubi333@gmail.com>
2022-11-28 17:01:47 -0800

    Fix duplicate gene columns issue

--- 01-combine/combine.py
+++ 01-combine/combine.py
@@ -38,34 +38,35 @@ for i in range(1, 6):
 
     df = pd.DataFrame.sparse.from_spmatrix(
         counts, index=barcodes, columns=features
-    ).drop(
-        columns=[
-            "RGS5",
-            "TBCE",
-            "PDE11A",
-            "LINC01238",
-            "PRSS50",
-            "CYB561D2",
-            "ATXN7",
-            "TXNRD3NB",
-            "CCDC39",
-            "MATR3",
-            "SOD2",
-            "POLR2J3",
-            "ABCF2",
-            "TMSB15B",
-            "PINX1",
-            "LINC01505",
-            "IGF2",
-            "HSPA14",
-            "EMG1",
-            "DIABLO",
-            "LINC02203",
-            "COG8",
-            "SCO2",
-            "H2BFS",
-        ]
     )
+    # .drop(
+    #     columns=[
+    #         "RGS5",
+    #         "TBCE",
+    #         "PDE11A",
+    #         "LINC01238",
+    #         "PRSS50",
+    #         "CYB561D2",
+    #         "ATXN7",
+    #         "TXNRD3NB",
+    #         "CCDC39",
+    #         "MATR3",
+    #         "SOD2",
+    #         "POLR2J3",
+    #         "ABCF2",
+    #         "TMSB15B",
+    #         "PINX1",
+    #         "LINC01505",
+    #         "IGF2",
+    #         "HSPA14",
+    #         "EMG1",
+    #         "DIABLO",
+    #         "LINC02203",
+    #         "COG8",
+    #         "SCO2",
+    #         "H2BFS",
+    #     ]
+    # )
 
     mdf_columns = {}
 
@@ -83,25 +84,57 @@ for i in range(1, 6):
     separated_data.append(df)
     separated_metadata.append(mdf)
 
-
-#%% Merge data and metadata
+#%% Clean up missing and duplicate genes
 
 genes = np.intersect1d(separated_data[0].columns, separated_data[1].columns)
+duplicate_tle_genes = [
+    "RGS5",
+    "TBCE",
+    "PDE11A",
+    "LINC01238",
+    "PRSS50",
+    "CYB561D2",
+    "ATXN7",
+    "TXNRD3NB",
+    "CCDC39",
+    "MATR3",
+    "SOD2",
+    "POLR2J3",
+    "ABCF2",
+    "TMSB15B",
+    "PINX1",
+    "LINC01505",
+    "IGF2",
+    "HSPA14",
+    "EMG1",
+    "DIABLO",
+    "LINC02203",
+    "COG8",
+    "SCO2",
+    "H2BFS",
+]
+
+for i in range(1, 6):
+    for gene in duplicate_tle_genes:
+        vals = separated_data[i][gene].values.sum(axis=1)
+        separated_data[i] = separated_data[i].drop(columns=[gene])
+        separated_data[i][gene] = pd.arrays.SparseArray(vals)
+
+#%% Merge data and metadata
 
 intersected_healthy_data = healthy_data[genes]
 intersected_tle_data = pd.concat(separated_data[1:], join="inner")[genes]
 
 tle_metadata = pd.concat(separated_metadata[1:])
 
-
 #%% Save data and metadata
 
-# intersected_healthy_data.to_csv("out/combined-healthy-data.csv")
-# healthy_metadata.to_csv("out/combined-healthy-metadata.csv")
+intersected_healthy_data.to_csv("out/combined-healthy-data.csv")
+healthy_metadata.to_csv("out/combined-healthy-metadata.csv")
 
-# scipy.io.mmwrite(
-#     "out/combined-tle-data.mtx", intersected_tle_data.sparse.to_coo()
-# )
-# tle_metadata.to_csv("out/combined-tle-metadata.csv")
+scipy.io.mmwrite(
+    "out/combined-tle-data.mtx", intersected_tle_data.sparse.to_coo()
+)
+tle_metadata.to_csv("out/combined-tle-metadata.csv")
 
 pd.DataFrame({"Gene": genes}).to_csv("out/combined-genes.csv", index=False)
